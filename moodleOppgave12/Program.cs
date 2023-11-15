736f1a48dc4775dc95248698914cd9e7b6a77df5
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 10:30:50 +0100

    Add project files.

---
+++ moodleOppgave12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace moodleOppgave12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isLessThan10 = true;
+
+            int counter = 0;
+
+            while (isLessThan10){
+                if (counter < 10)
+                {
+                    Console.WriteLine($"Runde {counter + 1}");
+                    counter++;
+                }
+                else
+                {
+                    isLessThan10 = false;
+                }
+            }
+        }
+    }
+}
